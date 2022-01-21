e60f0b220ddef038a5a51f2e4e03cba3bfa6347e
Mafee7 <64575804+Mafee6@users.noreply.github.com>
2022-01-21 09:31:48 +0530

    Add files via upload

--- Lizv1.0/pages/config.js
+++ Lizv1.0/pages/config.js
@@ -1,1 +1,13 @@
-
+const lizconf = {
+    pages: [
+        {
+          page: "home",
+          main: "index.html",
+          js: "index.js",
+          css: "index.css",
+          overflowblock: true,
+          nomargin: true,
+          favicon: "icons/favicon.png"
+        }
+    ]
+};
